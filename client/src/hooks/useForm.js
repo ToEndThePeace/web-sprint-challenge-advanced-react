145fd27ce04c43e9e283255175aadeb61e02d7c4
ToEndThePeace <bran.ramirez.don@gmail.com>
2020-05-08 10:58:51 -0700

    Added localstorage hook to form hook so user info persists on checkout page

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,8 +1,9 @@
 // write your custom hook here to control your checkout form
-import { useState } from "react";
+
+import useLocalStorage from "./useLocalStorage";
 
 const useForm = (initialValues) => {
-  const [formValues, setFormValues] = useState(initialValues);
+  const [formValues, setFormValues] = useLocalStorage("checkout", initialValues);
   const inputHandler = (e) => {
     const { name, value } = e.target;
     setFormValues({
